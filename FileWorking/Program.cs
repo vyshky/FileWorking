1197cb1f1d34e93f0edec6fa5aca60f4beca9013
Alexander Deyukhin <vyshky2@Gmail.com>
2021-08-28 12:01:34 +0400

    Считываем файл в переменную

---
+++ FileWorking/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.IO;
+
+namespace FileWorking
+{
+    class Program
+    {
+        static void Main()
+        {
+            var path_input = @"C:\Users\vyshk\RiderProjects\FileWorking\FileWorking\input.txt";
+            StreamReader file = new StreamReader(path_input);
+            var temp = file.ReadToEnd(); // в темп хранится все содержимое файла
+            file.Close();
+            
+            Console.WriteLine(temp);
+            
+        }
+    }
+}
