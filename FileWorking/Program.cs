e7978421169aafbb32fda6cbeb4f45e8dfd9853c
Alexander Deyukhin <vyshky2@Gmail.com>
2021-08-28 12:44:42 +0400

    Добавил считывание из файла с разделением данных

--- FileWorking/Program.cs
+++ FileWorking/Program.cs
@@ -20,10 +20,25 @@ namespace FileWorking
             user.DateOfBithday = new DateTime(1994, 03, 27);
 
             string output = $"{user.Name};{user.DateOfBithday.ToString("d")}";
-            using (StreamWriter file = new StreamWriter(path, false))
+            using (StreamWriter file = new StreamWriter(path, false)) //запись в файл из output
             {
                 file.WriteLine(output);
             }
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////
+            User person = new User();
+            string line;
+            using (StreamReader file = new StreamReader(path)) //считывание из файла в line
+            {
+                line = file.ReadLine();
+            }
+
+
+            //Console.WriteLine(line[0]);// выводит по одному символу
+            string[] temp = line.Split(";"); // расщепляем слова разделительным символом ";" 
+            person.Name = temp[0];
+            person.DateOfBithday = Convert.ToDateTime(temp[1]);
+            Console.WriteLine($"{user.Name};{user.DateOfBithday.ToString("d")}");
         }
     }
 }
